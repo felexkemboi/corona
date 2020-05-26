18b0214dc47ac535b86b511558d5f507ed9397c1
felexkemboi <felokemboi10@gmail.com>
2020-05-26 16:09:41 +0300

    final work

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,1 @@
-const form = document.querySelector('form');
 
-const formEvent = form.addEventListener('submit', event => {
-    event.preventDefault();
-
-    const first_name = document.querySelector('#first_name').value;
-    const last_name = document.querySelector('#last_name').value;
-
-    const user = { first_name, last_name };
-    console.log(user)
-});
